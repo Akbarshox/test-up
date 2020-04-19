7175646f4e5c6b627387d13dd8345035847cedf6
Akbarshox <akbarshax@bk.ru>
2020-04-19 20:04:46 +0500

    'addcart'

---
+++ build/precache-manifest.28a2ce428968f9443846a890f4a6e11d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73d89eb6b5d3cd1c945bb08032b43475",
+    "url": "/index.html"
+  },
+  {
+    "revision": "60cafbf535877eb05e11",
+    "url": "/static/css/main.c415b337.chunk.css"
+  },
+  {
+    "revision": "a1ce4b0edda6ece93b60",
+    "url": "/static/js/2.16c94d56.chunk.js"
+  },
+  {
+    "revision": "ac9fac6c8e755a8efecd01981adb88e6",
+    "url": "/static/js/2.16c94d56.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60cafbf535877eb05e11",
+    "url": "/static/js/main.de8a0ddd.chunk.js"
+  },
+  {
+    "revision": "d2e3d287abbff0085f2e",
+    "url": "/static/js/runtime-main.9d651b5c.js"
+  }
+]);
