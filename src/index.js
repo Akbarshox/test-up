cc86b86e54e2c02b5863afc3b00e5afbd6408b1d
Akbarshox <akbarshax@bk.ru>
2020-06-04 13:24:12 +0500

    'sw-precache

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ import './css/index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import {StoreProvider} from './Store';
- 
+
 ReactDOM.render(
     <StoreProvider>
-        <App />
+       <App/>
     </StoreProvider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
